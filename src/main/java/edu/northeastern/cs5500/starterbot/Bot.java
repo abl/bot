0d9b101a2bbaad2261c59440ade9b1b28d075457
Alexander Lash <167740+abl@users.noreply.github.com>
2023-04-17 18:08:39 -0700

    Factor JDA out into a Service

--- src/main/java/edu/northeastern/cs5500/starterbot/Bot.java
+++ src/main/java/edu/northeastern/cs5500/starterbot/Bot.java
@@ -8,15 +8,10 @@ import edu.northeastern.cs5500.starterbot.service.OpenTelemetryService;
 import edu.northeastern.cs5500.starterbot.service.ServiceModule;
 import io.opentelemetry.api.trace.SpanKind;
 import io.opentelemetry.context.Scope;
-import java.util.Collection;
-import java.util.EnumSet;
-import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 import net.dv8tion.jda.api.JDA;
-import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 
 @Component(modules = {CommandModule.class, RepositoryModule.class, ServiceModule.class})
@@ -33,32 +28,21 @@ public class Bot {
 
     @Inject MessageListener messageListener;
     @Inject OpenTelemetryService openTelemetryService;
+    @Inject JDA jda;
 
     static String getBotToken() {
         return new ProcessBuilder().environment().get("BOT_TOKEN");
     }
 
     void start() {
-        var span = openTelemetryService.span("start", SpanKind.PRODUCER);
+        var span = openTelemetryService.span("updateCommands", SpanKind.PRODUCER);
         try (Scope scope = span.makeCurrent()) {
-            String token = getBotToken();
-            if (token == null) {
-                throw new IllegalArgumentException(
-                        "The BOT_TOKEN environment variable is not defined.");
-            }
-            @SuppressWarnings("null")
-            @Nonnull
-            Collection<GatewayIntent> intents = EnumSet.noneOf(GatewayIntent.class);
-            JDA jda =
-                    JDABuilder.createLight(token, intents)
-                            .addEventListeners(messageListener)
-                            .build();
-
+            jda.addEventListener(messageListener);
             CommandListUpdateAction commands = jda.updateCommands();
             commands.addCommands(messageListener.allCommandData());
             commands.queue();
         } catch (Exception e) {
-            log.error("Unable to start the bot", e);
+            log.error("Unable to add message listeners", e);
             span.recordException(e);
         } finally {
             span.end();
